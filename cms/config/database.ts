ce5470d894fffa310db68e30d97cb90af315a612
Andy Sprague <asprague@apartmentlist.com>
2025-05-10 13:21:45 -0700

    make pg in prod and sqlite locally

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -1,27 +1,56 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
-    },
-    useNullAsDefault: true,
-  },
-});
+import path from 'path';
 
-export default ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+function getConnection(env: any, client: string) {
+  const connections = {
+    mysql: {
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+      },
     },
-    useNullAsDefault: true,
-  },
-});
+    postgres: {
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+      },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+    },
+  };
+  return connections[client];
+}
+
+export default ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+  const connection = getConnection(env, client);
+
+  return {
+    connection: {
+      client,
+      ...connection,
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
+
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+  const connection = getConnection(env, client);
+
+  return {
+    connection: {
+      client,
+      ...connection,
+      useNullAsDefault: true,
+    },
+  };
+};
